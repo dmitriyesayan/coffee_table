5a0ffb3ea6078dc1b6b5e6a8e5ca803b0ed8bd36
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-02-17 20:13:41 +0100

    added has_many

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :coffee_chat
+  has_many :messages
+  has_many :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :bookings
+  has_many :coffee_chats
+  has_many :messages
+  has_many :reviews, through: :bookings
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
