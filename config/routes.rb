600b769d313c1451c3cca979a0dd2fc328db1a51
Gina Slager <g.natasia@live.nl>
2022-02-24 21:32:08 +0100

    calender

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get 'experts', to: 'users#index'
-  get 'experts/:id', to: "users#show", as: :expert
+  # get 'experts', to: 'users#index'
+  # get 'experts/:id', to: "users#show", as: :expert
 
   get 'experts/:id/coffee_chats', to: 'coffee_chats#index'
   patch 'experts/:id/coffee_chats', to: 'coffee_chats#update'
   delete 'experts/:id/coffee_chats', to: 'coffee_chats#destroy'
 
-  post 'experts/:id/coffee_chats/:id/bookings', to: 'bookings#create'
-  get 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#show'
-  patch 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#update'
-  delete 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#destroy'
+  resources :experts, controller: 'users', only: [:index, :show] do
+    resources :bookings
+  end
+  # get 'experts/:id/coffee_chats/:id/bookings', to: 'bookings#new', as: 'new_booking'
+  # post 'experts/:id/coffee_chats/:id/bookings', to: 'bookings#create'
+  # get 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#show'
+  # patch 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#update'
+  # delete 'experts/:id/coffee_chats/:id/bookings/:id', to: 'bookings#destroy'
 
   get 'dashboard', to: 'pages#dashboard'
   get 'profile', to: 'pages#profile'
