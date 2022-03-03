c56a97d9fdc941142cc2f0a69a06275c2625abf7
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-03-03 21:04:37 +0100

    added user dashboard

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,5 +3,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.booking_id = params[:id]
+    if @review.save
+      redirect_to dashboard_path
+    else
+      raise
+    end
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
