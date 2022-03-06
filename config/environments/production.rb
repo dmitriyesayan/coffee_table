d83988285ae08d4d2c7b4085fb1b9379dc0839ef
Dmitri Yesayan <dmitri.yesayan@gmail.com>
2022-03-06 22:21:36 +0100

    added some gems and config to fix photo loading issue un heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,11 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # fix for photos in assets to load in heroku
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
