deaf2b18a7f77e1714d70d466ede6452082e8cd1
Jamie Hu <jamiehu@gmail.com>
2022-03-10 21:22:21 +0100

    homepageupdate

--- app/controllers/coffee_chats_controller.rb
+++ app/controllers/coffee_chats_controller.rb
@@ -12,18 +12,6 @@ class CoffeeChatsController < ApplicationController
     raise
   end
 
-  def create
-    coffee_chat_params.each do |param|
-      @coffee_chat = CoffeeChat.new(param)
-      @coffee_chat.user = current_user
-      @coffee_chat.availability = true
-      unless @coffee_chat.save
-        raise
-      end
-    end
-    redirect_to dashboard_path
-  end
-
   def update
   end
 
@@ -36,21 +24,6 @@ class CoffeeChatsController < ApplicationController
   private
 
   def coffee_chat_params
-    coffee_chat_param_array = []
-    date_range = params["date_range"].split(",")
-    array = []
-    date_range.each do |date|
-      param_hash = {
-      start: DateTime.new(date.split("-")[0].to_i,date.split("-")[1].to_i,date.split("-")[2].to_i,params["start_time"].split(":")[0].to_i,params["start_time"].split(":")[1].to_i,0),
-      end: DateTime.new(date.split("-")[0].to_i,date.split("-")[1].to_i,date.split("-")[2].to_i,params["end_time"].split(":")[0].to_i,params["end_time"].split(":")[1].to_i,0)
-    }
-    array << param_hash
-    end
-    array
+    params[:start, :end]
   end
-  
 end
-
-
-"start(1i)"=>"2022", "start(2i)"=>"3", "start(3i)"=>"10", "start(4i)"=>"16", "start(5i)"=>"41", "end(1i)"=>"2022", "end(2i)"=>"3", "end(3i)"=>"10", "end(4i)"=>"17", "end(5i)"=>"41", "commit"=>"Save", "controller"=>"coffee_chats", "action"=>"create"} permitted: false>
->>
