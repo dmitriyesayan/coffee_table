253fcd7314e98682b740b6d1d46f2b0154e954d4
abdollah farsane <abefarsane@gmail.com>
2022-03-15 16:29:11 +0100

    added content to about us and become an expert page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
 
 Rails.start()
 Turbolinks.start()
